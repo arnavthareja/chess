f86e43c53508c95f73506320a1caf623ebfbddc8
arnavthareja <arnav.thareja25@gmail.com>
2020-11-30 21:17:11 -0800

    Throw IllegalArgumentException in Board.squareAt()

--- src/chess/Square.java
+++ src/chess/Square.java
@@ -13,10 +13,8 @@ public class Square {
     }
 
     public Square(int row, int col, Board board, Piece piece) {
-        if (row < 0 || row > 7) {
-            throw new IllegalArgumentException("Row must be between 0 and 7");
-        } else if (col < 0 || col > 7) {
-            throw new IllegalArgumentException("Column must be between 0 and 7");
+        if (row < 0 || row > 7 || col < 0 || col > 7) {
+            throw new IllegalArgumentException("Row and column must be between 0 and 7");
         }
         this.row = row;
         this.col = col;
