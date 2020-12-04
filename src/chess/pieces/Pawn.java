1a4df0f63d769499325eeafde7af1def7f69c6f9
TylerD0808 <tyler.debrock@yahoo.com>
2020-12-03 20:39:41 -0800

    Add getPossibleMoves method to Pawn

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -12,15 +12,56 @@ public class Pawn extends Piece {
     }
 
     public Set<Move> getPossibleMoves() {
-        possibleMoves = new HashSet<>();
-        if (!alreadyMoved) {
-            // For first move
-        }
         // Check if piece of opposite color is diagonal. If so, can move there. If not, can't move there
         // Check if frontal position is blocked by anything. If so, can't move. If not, can move there
         // Can't go off board
+        Set<Move> possibleMoves = getStraightMoves();
+        possibleMoves.add(getDiagonalMoves(1, 1));
+        possibleMoves.add(getDiagonalMoves(-1, 1));
+        // possibleMoves.addAll(getDiagonalMoves());
         return possibleMoves;
     }
+
+    protected Set<Move> getStraightMoves() {
+        Set<Move> possibleMoves = new HashSet<>();
+        try {
+            Square finalPosition = position.getBoard().squareAt(position.getRow() + 1,
+                                                                position.getCol());
+            if (finalPosition.isEmpty()) {
+                possibleMoves.add(new Move(position, finalPosition));
+                finalPosition = finalPosition.getBoard().squareAt(finalPosition.getRow() + 1,
+                                                                  finalPosition.getCol());
+                if (!alreadyMoved && finalPosition.isEmpty()) {
+                    possibleMoves.add(new Move(position, finalPosition));
+                }
+            }
+        } catch (IllegalArgumentException e) {}
+        return possibleMoves;
+    }
+
+    private Move getDiagonalMoves(int dx, int dy) {
+        Move returnMove = null;
+        try {
+            Square finalPosition = position.getBoard().squareAt(position.getRow() + dy,
+                                                                position.getCol() + dx);
+            if (!finalPosition.isEmpty() && !finalPosition.getPiece().getColor().equals(color)) {
+                returnMove = new Move(position, finalPosition);
+            }
+        } catch (IllegalArgumentException e) {}
+        return returnMove;
+    }
+
+    // private Set<Move> getDiagonalMoves(int dx, int dy) {
+    //     Set<Move> possibleMoves = new HashSet<>();
+    //     try {
+    //         Square finalPosition = position.getBoard().squareAt(position.getRow() + dy,
+    //                                                             position.getCol() + dx);
+    //         if (!finalPosition.isEmpty() && !finalPosition.getPiece().getColor().equals(color)) {
+    //             possibleMoves.add(new Move(position, finalPosition));
+    //         }
+    //     } catch (IllegalArgumentException e) {}
+    //     return possibleMoves;
+    // }
 
     public void draw() {
         return;
