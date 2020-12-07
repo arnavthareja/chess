e9a75d454d24a1b36d101ac0b8ed63681f83c1f3
arnavthareja <arnav.thareja25@gmail.com>
2020-12-06 21:54:11 -0800

    Comment out unnecessary code

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -8,9 +8,9 @@ import java.util.*;
 public class Chess {
     public static void main(String[] args) {
         Board board = new Board();
-        //Player p1 = new HumanPlayer(board, Piece.Color.WHITE);
-        //Player p1 = new RandomPlayer(board, Piece.Color.WHITE, 0);
-        Player p1 = new MinimaxPlayer(board, Piece.Color.WHITE, new CombinationHeuristic());
+        Player p1 = new HumanPlayer(board, Piece.Color.WHITE);
+//        Player p1 = new RandomPlayer(board, Piece.Color.WHITE, 0);
+//        Player p1 = new MinimaxPlayer(board, Piece.Color.WHITE, new CombinationHeuristic());
         Player p2 = new MinimaxPlayer(board, Piece.Color.BLACK, new CombinationHeuristic());
         Player currentPlayer = p1;
         while(!p1.inCheckmate() && !p1.inStalemate() && !p2.inCheckmate()) {
@@ -19,10 +19,10 @@ public class Chess {
             currentPlayer = currentPlayer == p1 ? p2 : p1;
         }
         System.out.println(board);
-        Deque<Move> moves = board.getAllMoves();
-        while (!moves.isEmpty()) {
-            System.out.println(moves.pop());
-        }
+//        Deque<Move> moves = board.getAllMoves();
+//        while (!moves.isEmpty()) {
+//            System.out.println(moves.pop());
+//        }
         if (p1.inCheckmate()) {
             System.out.println("Black wins!");
         } else if (p2.inCheckmate()) {
--- src/chess/players/MinimaxPlayer.java
+++ src/chess/players/MinimaxPlayer.java
@@ -67,55 +67,55 @@ public class MinimaxPlayer extends Player {
             }
         }
         memo.put(allMoves, result);
-//        System.out.println("" + color + " " + " " + isMax + " " + result.first().getHeuristicValue() + " " + result.last().getHeuristicValue());
-//        System.out.println("Selected: " + (result.isEmpty() ? null : isMax ? result.last().getHeuristicValue() : result.first().getHeuristicValue()));
+//        System.out.println("Depth: " + depth + " " + color + " " + " " + result.first().getHeuristicValue() + " " + result.last().getHeuristicValue());
+//        System.out.println("Selected: " + (result.isEmpty() ? null : result.last().getHeuristicValue()));
         return result.isEmpty() ? null : result.last();
     }
 
-    // For DESIGN.md: used sorted set instead of priority queue because wanted to iterate over it without destroying it
-    private Move minimax(Piece.Color color, int depth, double alpha, double beta, boolean isMax) {
-        // Create a new deque to avoid weirdness with reference semantics
-        Deque<Move> allMoves = new ArrayDeque<>(board.getAllMoves());
-        Move start = allMoves.peek();
-        if (depth == 0) {
-            // If start is null here something went wrong. start should never be null
-            assert start != null;
-            start.calculateHeuristicValue(heuristic, board, color);
-            return start;
-        }
-        NavigableSet<Move> moves = memo.containsKey(allMoves) ? memo.get(allMoves) : new TreeSet<>(getPossibleMoves());
-        NavigableSet<Move> result = new TreeSet<>();
-        // This reverses getPossibleMoves too, but it doesn't matter as they're in random order (I think)
-        for (Move m : isMax ? moves.descendingSet() : moves) {
-            board.doMove(m);
-            Move next = minimax(oppositeColor(color),depth - 1, alpha, beta, !isMax);
-            double value;
-            if (next == null) {
-                if (board.inCheckmate(color)) {
-                    value = Double.NEGATIVE_INFINITY;
-                } else if (board.inCheckmate(oppositeColor(color))) {
-                    value = Double.POSITIVE_INFINITY;
-                } else {
-                    value = 0;
-                }
-            } else {
-                value = next.getHeuristicValue(); // Test this not being negated
-            }
-            m.setHeuristicValue(value);
-            result.add(m);
-            board.undoLastMove();
-            if (isMax) {
-                alpha = Math.max(alpha, m.getHeuristicValue());
-            } else {
-                beta = Math.min(beta, m.getHeuristicValue());
-            }
-            if (alpha >= beta) {
-                break;
-            }
-        }
-        memo.put(allMoves, result);
-//        System.out.println("" + color + " " + " " + isMax + " " + result.first().getHeuristicValue() + " " + result.last().getHeuristicValue());
-//        System.out.println("Selected: " + (result.isEmpty() ? null : isMax ? result.last().getHeuristicValue() : result.first().getHeuristicValue()));
-        return result.isEmpty() ? null : isMax ? result.last() : result.first();
-    }
+//    // For DESIGN.md: used sorted set instead of priority queue because wanted to iterate over it without destroying it
+//    private Move minimax(Piece.Color color, int depth, double alpha, double beta, boolean isMax) {
+//        // Create a new deque to avoid weirdness with reference semantics
+//        Deque<Move> allMoves = new ArrayDeque<>(board.getAllMoves());
+//        Move start = allMoves.peek();
+//        if (depth == 0) {
+//            // If start is null here something went wrong. start should never be null
+//            assert start != null;
+//            start.calculateHeuristicValue(heuristic, board, color);
+//            return start;
+//        }
+//        NavigableSet<Move> moves = memo.containsKey(allMoves) ? memo.get(allMoves) : new TreeSet<>(getPossibleMoves());
+//        NavigableSet<Move> result = new TreeSet<>();
+//        // This reverses getPossibleMoves too, but it doesn't matter as they're in random order (I think)
+//        for (Move m : isMax ? moves.descendingSet() : moves) {
+//            board.doMove(m);
+//            Move next = minimax(oppositeColor(color),depth - 1, alpha, beta, !isMax);
+//            double value;
+//            if (next == null) {
+//                if (board.inCheckmate(color)) {
+//                    value = Double.NEGATIVE_INFINITY;
+//                } else if (board.inCheckmate(oppositeColor(color))) {
+//                    value = Double.POSITIVE_INFINITY;
+//                } else {
+//                    value = 0;
+//                }
+//            } else {
+//                value = next.getHeuristicValue(); // Test this not being negated
+//            }
+//            m.setHeuristicValue(value);
+//            result.add(m);
+//            board.undoLastMove();
+//            if (isMax) {
+//                alpha = Math.max(alpha, m.getHeuristicValue());
+//            } else {
+//                beta = Math.min(beta, m.getHeuristicValue());
+//            }
+//            if (alpha >= beta) {
+//                break;
+//            }
+//        }
+//        memo.put(allMoves, result);
+////        System.out.println("" + color + " " + " " + isMax + " " + result.first().getHeuristicValue() + " " + result.last().getHeuristicValue());
+////        System.out.println("Selected: " + (result.isEmpty() ? null : isMax ? result.last().getHeuristicValue() : result.first().getHeuristicValue()));
+//        return result.isEmpty() ? null : isMax ? result.last() : result.first();
+//    }
 }
