1871160b1eb5bb95d4e7da67cb42a9a51c7d0a2b
arnavthareja <arnav.thareja25@gmail.com>
2020-12-06 17:05:16 -0800

    Add RandomPlayer class

---
+++ src/chess/players/RandomPlayer.java
@@ -0,0 +1,33 @@
+package chess.players;
+
+import chess.*;
+import chess.pieces.*;
+import java.util.*;
+
+public class RandomPlayer extends Player {
+    private final Random random;
+
+    public RandomPlayer(Board board, Piece.Color color) {
+        this(board, color, new Random());
+    }
+
+    public RandomPlayer(Board board, Piece.Color color, long seed) {
+        this(board, color, new Random(seed));
+    }
+
+    private RandomPlayer(Board board, Piece.Color color, Random random) {
+        super(board, color);
+        this.random = random;
+    }
+
+    public Move getMove() {
+        Set<Move> possibleMoves = getPossibleMoves();
+        int index = random.nextInt(possibleMoves.size());
+        for (Move m : possibleMoves) {
+            if (index-- == 0) {
+                return m;
+            }
+        }
+        return null;
+    }
+}
