ab95c09cadbdf6b52e998ad6a7d58eb6a053dfa3
arnavthareja <arnav.thareja25@gmail.com>
2020-12-06 21:21:27 -0800

    Improve material heuristic

--- src/chess/heuristics/MaterialHeuristic.java
+++ src/chess/heuristics/MaterialHeuristic.java
@@ -7,14 +7,14 @@ import java.util.*;
 
 public class MaterialHeuristic implements Heuristic {
     public double calculateValue(Board board, Piece.Color color) {
-        return (evaluate(board, color) - evaluate(board, oppositeColor(color))) * color.getMultiplier();
+        return (evaluate(board, color) - evaluate(board, oppositeColor(color)));// * color.getMultiplier();
     }
 
     private double evaluate(Board board, Piece.Color color) {
         Set<Piece> pieces = board.getPieces(color);
         double numPiecesAdv = pieces.size() * 0.1; // weight by 0.1 (arbitrary) as number of points matter more
         double materialAdv = pieces.stream().mapToDouble(Piece::getValue).sum();
-        double numMovesAdv = board.getPossibleMoves(color).size() * 0.5; // weight mobility at 0.5 (arbitrary)
+        double numMovesAdv = board.getPossibleMoves(color).size() * 0.1; // weight mobility at 0.1 (arbitrary) as it may lead to aggressive playing
         return numPiecesAdv + materialAdv + numMovesAdv;
     }
 }
