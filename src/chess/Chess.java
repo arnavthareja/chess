350fd29dc0c76b7e4a38691aa7352a2e2f6dc2a7
arnavthareja <arnav.thareja25@gmail.com>
2020-12-07 22:18:45 -0800

    Update main play method to use human and minimax players

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -7,8 +7,7 @@ import chess.players.*;
 public class Chess {
     public static void main(String[] args) {
         Board board = new Board();
-//        Player p1 = new HumanPlayer(board, Piece.Color.WHITE);
-        Player p1 = new MinimaxPlayer(board, Piece.Color.WHITE, new CombinationHeuristic());
+        Player p1 = new HumanPlayer(board, Piece.Color.WHITE);
         Player p2 = new MinimaxPlayer(board, Piece.Color.BLACK, new CombinationHeuristic());
         Player currentPlayer = p1;
         while(!p1.inCheckmate() && !p1.inStalemate() && !p2.inCheckmate()) {
@@ -25,8 +24,6 @@ public class Chess {
                 System.out.print("Black");
             }
             System.out.println(" to move");
-            // The above 5 lines used to be the commented line below, but it changed behavior for whatever reason
-            // System.out.println((currentPlayer == p1 ? "White" : "Black") + " to move");
             currentPlayer.doMove();
             currentPlayer = currentPlayer == p1 ? p2 : p1;
         }
